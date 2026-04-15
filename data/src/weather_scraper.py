7436ee7f2b803e70f3c9b2dd62ef2f58eb49a3b4
Daniel C. Velázquez <peli.covarrubias@gmail.com>
2026-04-15 15:53:42 -0600

    weather_scraper.py

--- data/src/weather_scraper.py
+++ data/src/weather_scraper.py
@@ -1,1 +1,13 @@
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
 
+url = "https://practicum-content.s3.us-west-1.amazonaws.com/data-analyst-eng/moved_chicago_weather_2017.html"
+
+response = requests.get(url)
+soup = BeautifulSoup(response.text, "html.parser")
+
+table = soup.find("table", attrs={"id": "weather_records"})
+weather_records = pd.read_html(str(table))[0]
+
+print(weather_records.head())
